278d59ae313f2e6b0ad1e79fc569734fb3638dd0
William Franklin <wilfrank84@gmail.com>
2023-11-28 15:17:50 -0300

    feat: layout improviments

--- apps/mfe-statements/src/app/page.tsx
+++ apps/mfe-statements/src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
       <Header />
       <main className="flex h-[calc(100vh-100px)] flex-col items-center p-24 bg-slate-100">
         <h1>Hello Statements</h1>
-        <small>(apps/mfe-statements)</small>
+        <small className="text-[11px] font-semibold">
+          (apps/mfe-statements)
+        </small>
       </main>
     </Fragment>
   );
--- packages/ui/src/header.tsx
+++ packages/ui/src/header.tsx
@@ -1,8 +1,8 @@
 export function Header(): JSX.Element {
   return (
-    <header className="ui-shadow-lg ui-p-6 ui-border-b-[1px]">
+    <header className="ui-shadow-lg ui-p-4 ui-border-b-[1px]">
       <h1>Header</h1>
-      <small className="">(packages/ui)</small>
+      <small className="ui-text-[11px] ui-font-semibold">(packages/ui)</small>
     </header>
   );
 }
