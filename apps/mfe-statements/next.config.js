88f0b17f6628ab9c619d56514ec096eeeda4c78c
William Franklin <wilfrank84@gmail.com>
2023-11-29 00:26:32 -0300

    feat: add multi zones config

--- apps/mfe-statements/next.config.js
+++ apps/mfe-statements/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      /**
+       * Rewrites for Multi Zones
+       */
+      {
+        source: "/transfers",
+        destination: `${TRANSFERS_URL}/transfers`,
+      },
+      {
+        source: "/transfers/:path*",
+        destination: `${TRANSFERS_URL}/transfers/:path*`,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
