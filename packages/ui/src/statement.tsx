3b1fa3aac978f7e71e577753996e32969c577781
William Franklin <wilfrank84@gmail.com>
2023-11-29 08:22:53 -0300

    feat: style improviments

--- packages/ui/src/statement.tsx
+++ packages/ui/src/statement.tsx
@@ -4,17 +4,17 @@ export function Statement(): JSX.Element {
   const addToBalance = useStore((state) => state.addToBalance);
 
   return (
-    <section className="ui-justify-center ui-p-8">
-      <h1 className="ui-flex ui-flex-col">
+    <section className="ui-justify-center ui-p-8 ui-flex-1">
+      <h1 className="ui-flex ui-flex-col ui-text-center">
         Statement
         <small className="ui-text-[11px] ui-font-semibold">
           (apps/mfe-statements)
         </small>
       </h1>
 
-      <section>
+      <section className="ui-text-center">
         <button
-          className="ui-bg-blue-600 ui-text-white ui-inline-block ui-my-3 ui-rounded ui-py-3 ui-px-4 ui-text-xs"
+          className="ui-bg-blue-600 ui-text-white ui-inline-block w-[200px] ui-my-3 ui-rounded ui-py-3 ui-px-4 ui-text-xs"
           onClick={() => {
             addToBalance(100);
           }}
