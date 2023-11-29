984aef12ea402a3303bf477684335b657a8be445
William Franklin <wilfrank84@gmail.com>
2023-11-29 01:06:50 -0300

    feat: menu styles

--- packages/ui/src/menu.tsx
+++ packages/ui/src/menu.tsx
@@ -3,13 +3,19 @@ export function Menu(): JSX.Element {
     <nav className="ui-h-[calc(100vh-90px)] ui-bg-white ui-inline-block ui-border-r-[1px] ui-min-w-[200px]">
       <ul>
         <li>
-          <a className="ui-p-3 ui-border-b-[1px] ui-block" href="/">
-            Home
+          <a
+            className="ui-p-3 ui-border-b-[1px] ui-block ui-font-medium"
+            href="/"
+          >
+            › Home
           </a>
         </li>
         <li>
-          <a className="ui-p-3 ui-border-b-[1px] ui-block" href="/transfers">
-            Transferências
+          <a
+            className="ui-p-3 ui-border-b-[1px] ui-block ui-font-medium"
+            href="/transfers"
+          >
+            › Transferências
           </a>
         </li>
       </ul>
